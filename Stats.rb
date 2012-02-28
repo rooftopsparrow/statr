8b5c7ac5c9d823c3f3864f86274686f928d316a0
Jonathan Nicholson <jnnyboy570@gmail.com>
2012-02-27 18:45:52 -0700

    Correlation method of a table 2x2

--- Stats.rb
+++ Stats.rb
@@ -102,4 +102,29 @@ class Table
     @table[:data]
   end
   
+  def s(arg, sum=true)
+    v = Vector[]
+    if arg == 'xx'
+      x = y = @table[:data][0]
+    elsif arg == 'yy'
+      x = y = @table[:data][1]
+    elsif arg == 'xy'
+      x, y = @table[:data]
+    else
+      raise "Options include: 'xx', 'yy', or 'xy'"
+    end
+    xbar, ybar = x.mean, y.mean
+    for xi, yi in x.zip(y)
+      v.push((xi-xbar)*(yi-ybar))
+    end
+    if sum
+      v.sum
+    else
+      v
+    end
+  end
+  
+  def correlate
+    self.s('xy') / Math.sqrt(self.s('xx')*self.s('yy'))
+  end
 end
